ec9265840d2c716dd2d4384c183c02c287a2b298
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2022-08-29 17:13:26 -0300

    Create bdd.sql

---
+++ bdd.sql
@@ -0,0 +1,15 @@
+
+drop schema if exists registro;
+use registro;
+CREATE TABLE `usuario` (
+  `nombre` varchar(12) NOT NULL DEFAULT '...',
+  `ci` int NOT NULL,
+  `fecha_nac` date NOT NULL DEFAULT '1900-01-01',
+  `email` varchar(60) NOT NULL DEFAULT '...',
+  `hash_1` varchar(100) DEFAULT NULL,
+  `hash_2` varchar(100) DEFAULT NULL,
+  `sal` varchar(100) DEFAULT NULL,
+  `marcaTiempo` bigint DEFAULT NULL,
+  PRIMARY KEY (`ci`),
+  UNIQUE KEY `ci` (`ci`)
+)
